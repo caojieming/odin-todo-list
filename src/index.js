79e431e22f4e9fcb747ddcf24a3c8bd2dfcb8965
James Cao <jcao5360@gmail.com>
2025-10-06 16:26:56 -0400

    implemented creating and deleting projects

--- src/index.js
+++ src/index.js
@@ -24,10 +24,13 @@ Todo list, roughly in order:
 
 class Controller {
 
+    projectsDiv = document.querySelector("#projects");
     sidebarDiv = document.querySelector("#sidebar");
+    contentDiv = document.querySelector("#content");
 
     constructor() {
         this.projects = [];
+
         const firstProject = new Project("First project", "This is the first project.");
         firstProject.addTodo("task 1", "look at enemy", format(new Date(2014, 1, 11), "MM/dd/yyyy"), 10);
         firstProject.addTodo("task 2", "panic roll", format(new Date(2016, 2, 19), "MM/dd/yyyy"), 3);
@@ -42,24 +45,76 @@ class Controller {
         
         this.addProject(secondProject);
 
-        this.sidebarDiv.addEventListener("click", this.openProjectContent.bind(this));
 
+
+        // click project tab on sidebar -> open project contents 
+        this.projectsDiv.addEventListener("click", this.openProjectContent.bind(this));
+
+        // click "+" on sidebar (create project button) -> open create project content
         const createProjectBtn = document.querySelector("#create-project-btn");
         createProjectBtn.addEventListener("click", this.createProject.bind(this));
+
+        // click Delete on sidebar -> delete project from sidbar and projects[]
+        this.projectsDiv.addEventListener("click", this.deleteProject.bind(this));
+
 
         
     }
 
 
+
+    deleteProject(event) {
+        const element = event.target;
+        // exits function if element is not a delete-project-btn
+        if(element.className !== "delete-project-btn") { return; }
+
+        const projectID = element.id;
+        const projectIdx = this.getProjectIndexFromID(projectID);
+
+        // delete project from projects[]
+        this.projects.splice(projectIdx, 1);
+
+        // remove from UI
+        this.removeChildAtIndex(this.projectsDiv, projectIdx);
+        
+        // also reset content if project was just deleted
+        const currContentTitle = document.querySelector(".project-title");
+        if(currContentTitle.id === projectID) {
+            this.contentDiv.textContent = '';
+        }
+    }
+    removeChildAtIndex(parentElement, index) {
+        const children = parentElement.children;
+        if(children.length > 0) {
+            parentElement.removeChild(children[index]);
+        }
+    }
+
+
+
     // opening the project creation UI onto content
     createProject() {
         new CreateProject();
+
+        // click finish button on create project content -> add project to sidebar and projects[]
+        const finishProjectBtn = document.querySelector("#finish-project-btn");
+        finishProjectBtn.addEventListener("click", this.finishProject.bind(this));
+    }
+    finishProject() {
+        const projTitle = this.contentDiv.querySelector("#project-title-input").value;
+        const projDescription = this.contentDiv.querySelector("#project-description-input").value;
+
+        const newProject = new Project(projTitle, projDescription);
+        this.addProject(newProject);
     }
 
 
     // opening a project onto content from the sidebar
     openProjectContent(event) {
         const element = event.target;
+        // exits function if element is not a project-btn
+        if(element.className !== "project-btn") { return; }
+
         const projectID = element.id;
         const projectIdx = this.getProjectIndexFromID(projectID);
 
--- src/project-sidebar.js
+++ src/project-sidebar.js
@@ -5,11 +5,24 @@ export const ProjectSidebar = class ProjectSidebar {
     projectsDiv = document.querySelector("#projects");
 
     constructor(project) {
-        const projectTab = document.createElement("button");
+        const projectTab = document.createElement("div");
         projectTab.classList.add("project-tab");
         projectTab.setAttribute("id", project.id);
-        projectTab.textContent = project.title;
         this.projectsDiv.appendChild(projectTab);
+        
+        // delete button
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("delete-project-btn");
+        deleteBtn.setAttribute("id", project.id);
+        deleteBtn.textContent = "Delete";
+        projectTab.appendChild(deleteBtn);
+
+        // project tab
+        const projectBtn = document.createElement("button");
+        projectBtn.classList.add("project-btn");
+        projectBtn.setAttribute("id", project.id);
+        projectBtn.textContent = project.title;
+        projectTab.appendChild(projectBtn);
     }
-    
+
 }
